90cab72c2b9b76edc6ad29c97584f1ce1bea6001
crowdbotics-apps <team@crowdbotics.com>
2023-08-09 12:13:19 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Horoscope
+admin.site.register(Horoscope)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+class Horoscope(models.Model):
+    'Generated Model'
+    title = models.CharField(max_length=255,)
+    slug = models.CharField(max_length=255,)
+    icon = models.CharField(max_length=255,)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    last_updated = models.DateField()
