22686ccdfd78d4692c764b8c8927e65e7a7dfd40
Saad bin Abid <saad.abid@crowdbotics.com>
2023-08-09 17:12:20 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "demo-platform-42740",
-  url: "https://demo-platform-42740.botics.co",
-  api: "https://demo-platform-42740.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "demo-platform-42740",
+  "url": "https://demo-platform-42740.botics.co",
+  "api": "https://demo-platform-42740.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI2"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
