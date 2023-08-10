96532220b0a976eee24a11f14afa8ebe887999c2
Saad bin Abid <saad.abid@crowdbotics.com>
2023-08-10 15:10:50 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/demoplatformAPI/api.js
+++ store/demoplatformAPI/api.js
@@ -1,76 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const demoplatformAPI = axios.create({
   baseURL: "https://demo-platform-42740.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return demoplatformAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_horoscope_list(payload) {
-  return demoplatformAPI.get(`/api/v1/horoscope/`)
+  return demoplatformAPI.get(`/api/v1/horoscope/`);
 }
+
 function api_v1_horoscope_create(payload) {
-  return demoplatformAPI.post(`/api/v1/horoscope/`, payload.data)
+  return demoplatformAPI.post(`/api/v1/horoscope/`, payload.data);
 }
+
 function api_v1_horoscope_retrieve(payload) {
-  return demoplatformAPI.get(`/api/v1/horoscope/${payload.id}/`)
+  return demoplatformAPI.get(`/api/v1/horoscope/${payload.id}/`);
 }
+
 function api_v1_horoscope_update(payload) {
-  return demoplatformAPI.put(`/api/v1/horoscope/${payload.id}/`, payload.data)
+  return demoplatformAPI.put(`/api/v1/horoscope/${payload.id}/`, payload.data);
 }
+
 function api_v1_horoscope_partial_update(payload) {
-  return demoplatformAPI.patch(`/api/v1/horoscope/${payload.id}/`, payload.data)
+  return demoplatformAPI.patch(`/api/v1/horoscope/${payload.id}/`, payload.data);
 }
+
 function api_v1_horoscope_destroy(payload) {
-  return demoplatformAPI.delete(`/api/v1/horoscope/${payload.id}/`)
+  return demoplatformAPI.delete(`/api/v1/horoscope/${payload.id}/`);
 }
+
 function api_v1_login_create(payload) {
-  return demoplatformAPI.post(`/api/v1/login/`, payload.data)
+  return demoplatformAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return demoplatformAPI.post(`/api/v1/signup/`, payload.data)
+  return demoplatformAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return demoplatformAPI.post(`/rest-auth/login/`, payload.data)
+  return demoplatformAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return demoplatformAPI.get(`/rest-auth/logout/`)
+  return demoplatformAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return demoplatformAPI.post(`/rest-auth/logout/`)
+  return demoplatformAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return demoplatformAPI.post(`/rest-auth/password/change/`, payload.data)
+  return demoplatformAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return demoplatformAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return demoplatformAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return demoplatformAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return demoplatformAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return demoplatformAPI.post(`/rest-auth/registration/`, payload.data)
+  return demoplatformAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return demoplatformAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return demoplatformAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return demoplatformAPI.get(`/rest-auth/user/`)
+  return demoplatformAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return demoplatformAPI.put(`/rest-auth/user/`, payload.data)
+  return demoplatformAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return demoplatformAPI.patch(`/rest-auth/user/`, payload.data)
+  return demoplatformAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_horoscope_list,
@@ -92,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
