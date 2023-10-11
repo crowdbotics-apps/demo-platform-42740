888bb660603fd075ab34d93216738fc05a3f1f9f
crowdbotics-apps <team@crowdbotics.com>
2023-10-11 18:57:34 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Item(models.Model):
     'Generated Model'
     item_name = models.CharField(max_length=255,)
+    price = models.BigIntegerField(null=True,blank=True,)
+    total = models.BigIntegerField(null=True,blank=True,)
